05efbf082600846029055115953333793ef0e834
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-01-15 12:00:44 +0000

    Updated the name of the default JS package

---
+++ assets/js/package-main.js
@@ -0,0 +1,8 @@
+//= require vendor/jquery.min
+//= require vendor/bootstrap.min
+//= require vendor/cookieconsent.min
+//= require vendor/lazysizes.min
+
+//= require app/main
+//= require app/tables
+//= require vendor/mc-validate
