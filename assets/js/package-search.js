502b29cb686911d5f7b6e7299a65d2ba714c7261
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-01-17 16:09:27 +0000

    Removed site specific JS packages

--- assets/js/package-search.js
+++
@@ -1,9 +0,0 @@
-//= require vendor/jquery.min
-//= require vendor/bootstrap.min
-//= require vendor/cookieconsent.min
-//= require vendor/lazysizes.min
-
-//= require app/main
-//= require app/search-page
-//= require app/tables
-
