5524075cd9dbb804bbcd52f7c743f346f29f1b63
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-04-24 11:45:13 +0100

    Updated search.scss and package-search.js

--- assets/js/package-search.js
+++
@@ -1,8 +0,0 @@
-//= require vendor/jquery
-//= require vendor/bootstrap
-//= require vendor/cookieconsent
-//= require vendor/lazysizes
-
-//= require app/main
-//= require app/tables
-//= require app/custom
