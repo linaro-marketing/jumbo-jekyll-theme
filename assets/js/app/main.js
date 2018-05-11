5fc502cce25a7f4a0b5e41b57202e78ee86d4c46
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-05-11 13:17:59 +0100

    Updated the bootstrap tabs JS for permalinks in the url

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -79,9 +79,8 @@ $(document).ready(function () {
     
     // Enabled permalinks to specific Bootstrap tabs
     var hash = document.location.hash;
-    var prefix = "tab_";
     if (hash) {
-        $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+        $('.nav-tabs a[href="'+hash+'"]').tab('show');
     } 
     // Change hash for page-reload
     $('.nav-tabs a').on('shown', function (e) {
