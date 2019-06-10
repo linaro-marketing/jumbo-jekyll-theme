e3f34f9b2aa28271ae6524fa96df976d826ab2c6
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-06-10 12:00:24 +0100

    Adding the original JS packages

---
+++ assets/js/package-home.js
@@ -0,0 +1,14 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/owl.carousel
+//= require vendor/jquery.doubleScroll
+//= require vendor/featherlight
+
+//= require app/main
+//= require app/tables
+//= require app/search
+//= require app/sticky-tab-bar
+//= require app/custom
+//= require app/home
