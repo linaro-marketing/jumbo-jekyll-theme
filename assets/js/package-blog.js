98625c1a501436bfe6974019fb96e26383118c15
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-06-19 12:10:55 +0100

    Adding original files

---
+++ assets/js/package-blog.js
@@ -0,0 +1,13 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+//= require vendor/jquery.doubleScroll
+//= require vendor/featherlight
+//= require vendor/clipboard.min
+
+//= require app/main
+//= require app/search
+//= require app/tables
+//= require app/custom
---
+++ assets/js/package-main.js
@@ -0,0 +1,15 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+//= require vendor/jquery.doubleScroll
+//= require vendor/owl.carousel
+//= require vendor/featherlight
+
+//= require app/main
+//= require app/tables
+//= require app/search
+//= require app/fly
+//= require app/sticky-tab-bar
+//= require app/custom
